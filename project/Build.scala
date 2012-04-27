d2b69d4fd7a30f3b64de368eb110abffcc8ea113
Ndidi Alaneme <ndidia@cakesolutions.net>
2012-04-27 10:05:53 +0100

    Deadbolt-2 intro

--- project/Build.scala
+++ project/Build.scala
@@ -9,17 +9,19 @@ object ApplicationBuild extends Build {
 
 	val sbtIdeaRepo		= "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
 
-    val appDependencies = Seq(
-      "org.neo4j" % "neo4j" % "1.7.M01",
-      "com.sun.jersey" % "jersey-client" % "1.12",
-      "net.databinder" %% "dispatch-http" % "0.8.8" withSources
-    )
+  val appDependencies = Seq(
+    "org.neo4j" % "neo4j" % "1.7.M01",
+    "com.sun.jersey" % "jersey-client" % "1.12",
+    "net.databinder" %% "dispatch-http" % "0.8.8" withSources,
+    "be.objectify" % "deadbolt-2_2.9.1" % "1.1.3-SNAPSHOT"
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here
-	  	resolvers ++= Seq(
-			sbtIdeaRepo
-		)
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    // Add your own project settings here
+    resolvers ++= Seq(
+    sbtIdeaRepo,
+    Resolver.url("Objectify Play Repository", url("http://schaloner.github.com/releases/"))(Resolver.ivyStylePatterns),
+    Resolver.url("Objectify Play Repository", url("http://schaloner.github.com/snapshots/"))(Resolver.ivyStylePatterns)
     )
-
+  )
 }
